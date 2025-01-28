6c98451e8d46792ad6439efd828a2bfeda1cb9cd
Sondre Sørbye <sondre@magson.no>
2025-01-28 11:47:30 +0100

    test model

--- test_model.py
+++ test_model.py
@@ -1,5 +1,7 @@
 import torch
 import wandb
+from torch import nn
+
 from dataloader import get_data_loader
 from hyperparameters import hyperparameters
 from models.transformer import Transformer
@@ -7,28 +9,28 @@ from train import load_checkpoint
 from validate import validate
 from vocab import load_vocab
 
-def main():
+
+def main() -> None:
     # Load model from wandb
     wandb.restore("checkpoints/checkpoint-500000.pth", run_path="sondresorbye-magson/TransformerUQ/5k0r04m7")
     en_vocab = load_vocab("local/vocab_en.pkl")
     de_vocab = load_vocab("local/vocab_de.pkl")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Transformer(
+    model: nn.Module = Transformer(
         src_vocab_size=len(de_vocab),
         tgt_vocab_size=len(en_vocab),
-        d_model=hyperparameters.encoder_embed_dim,
-        num_heads=hyperparameters.encoder_attention_heads,
-        d_ff=hyperparameters.encoder_ffn_embed_dim,
-        num_encoder_layers=hyperparameters.encoder_layers,
-        num_decoder_layers=hyperparameters.encoder_layers,
-        dropout=hyperparameters.dropout,
-        max_len=hyperparameters.max_len,
+        d_model=hyperparameters.transformer.encoder_embed_dim,
+        num_heads=hyperparameters.transformer.encoder_attention_heads,
+        d_ff=hyperparameters.transformer.encoder_ffn_embed_dim,
+        num_encoder_layers=hyperparameters.transformer.encoder_layers,
+        num_decoder_layers=hyperparameters.transformer.encoder_layers,
+        dropout=hyperparameters.transformer.dropout,
+        max_len=hyperparameters.transformer.max_len,
     ).to(device)
     model = torch.compile(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     load_checkpoint(model, optimizer, "checkpoints/checkpoint-500000.pth")
 
-    max_len=512
     test_loader = get_data_loader(
         src_file="local/data/test/bpe_test.de",
         tgt_file="local/data/test/bpe_test.en",
@@ -37,10 +39,11 @@ def main():
         batch_size=64,
         add_bos_eos=True,
         shuffle=False,
-        max_len=max_len,
+        max_len=hyperparameters.transformer.max_len,
     )
 
-    validate(model, test_loader, None)
+    bleu = validate(model, test_loader, None)
+    print(f"BLEU Score: {bleu}")
 
 
 if __name__ == '__main__':
