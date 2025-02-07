07238493a1f0344302a58226094501a79bdb3807
JonahNiklas <jonahwiecek@gmail.com>
2025-02-07 13:14:03 +0100

    feat: add devset to pipeline

--- constants.py
+++ constants.py
@@ -17,10 +17,19 @@ class FilePaths(BaseModel):
     tokenized_ood_en: str = "local/data/test_ood/tokenized_test_ood.en"
     tokenized_ood_nl: str = "local/data/test_ood/tokenized_test_ood.nl"
 
+    dev_en: str = "local/data/dev/dev.en"
+    dev_de: str = "local/data/dev/dev.de"
+    tokenized_dev_en: str = "local/data/dev/tokenized_dev.en"
+    tokenized_dev_de: str = "local/data/dev/tokenized_dev.de"
+
     bpe_train_en: str = "local/data/training/bpe_train.en"
     bpe_train_de: str = "local/data/training/bpe_train.de"
     bpe_test_en: str = "local/data/test/bpe_test.en"
     bpe_test_de: str = "local/data/test/bpe_test.de"
+    bpe_test_ood_en: str = "local/data/test_ood/bpe_test_ood.en"
+    bpe_test_ood_nl: str = "local/data/test_ood/bpe_test_ood.nl"
+    bpe_dev_en: str = "local/data/dev/bpe_dev.en"
+    bpe_dev_de: str = "local/data/dev/bpe_dev.de"
     
 
 
