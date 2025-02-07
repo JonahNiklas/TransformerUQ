ccb1a255743d9f25a42df02e3d4a982979487719
Sondre Sørbye <sondre@magson.no>
2025-02-07 13:50:49 +0100

    fix: update BayesMultiheadAttention to correctly calculate d_k and adjust linear layers for query, key, and value projections

--- models/bayesformer.py
+++ models/bayesformer.py
@@ -16,11 +16,11 @@ class BayesMultiheadAttention(nn.Module):
 
         self.d_model = d_model
         self.num_heads = num_heads
-        self.d_k = d_model
+        self.d_k = d_model // num_heads
 
-        self.W_q = nn.Linear(d_model, d_model * num_heads)
-        self.W_k = nn.Linear(d_model, d_model * num_heads)
-        self.W_v = nn.Linear(d_model, d_model * num_heads)
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
 
         self.out = nn.Linear(d_model, d_model)
 
