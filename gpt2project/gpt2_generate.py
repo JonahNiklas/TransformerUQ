5815ac8e0eb73b5e29d656563a496d5f7463045e
Sondre Sørbye <sondre@magson.no>
2025-03-05 14:28:47 +0100

    chore: formatting

--- gpt2project/gpt2_generate.py
+++ gpt2project/gpt2_generate.py
@@ -6,7 +6,11 @@ from torch import nn
 from typing import List, Tuple
 
 from gpt2project.gpt2model import GPT
-from gpt2project.search_methods_gpt import AutoregressiveInferenceResults, GPT_search_method, topk_sampling_gpt
+from gpt2project.search_methods_gpt import (
+    AutoregressiveInferenceResults,
+    GPT_search_method,
+    topk_sampling_gpt,
+)
 from gpt2project.utils.decode import decode_token_id_batch, decode_token_list
 from hyperparameters import hyperparameters
 from torch.functional import F
@@ -60,18 +64,22 @@ def generate_autoregressivly_gpt2_with_uq(
     # mean_hypothesis: List[str] = []
 
     for n in range(hyperparameters.uq.num_inferences):
-        output = search_method(model, tgt_tokens, vocab_size, max_tokens, break_on_newline)
+        output = search_method(
+            model, tgt_tokens, vocab_size, max_tokens, break_on_newline
+        )
         assert output.token_ids.shape == (batch_size, max_tokens)
 
         token_ids[:, n, :] = output.token_ids
         softmax_probs[:, n, :] = output.get_softmax_probs_for_selected_token()
         for b in range(batch_size):
-            hypothesis[b].append(decode_token_list(output.token_ids[b].tolist(), tokenizer))
+            hypothesis[b].append(
+                decode_token_list(output.token_ids[b].tolist(), tokenizer)
+            )
 
     for i, aq_func in enumerate(aq_funcs):
         uq = aq_func(hypothesis, token_ids, softmax_probs)
         uqs[:, i] = uq
-        # TODO: mean hypothesis instead of first 
+        # TODO: mean hypothesis instead of first
         # if aq_func.multiple_inference:
         #     hyp = BLEU_mean_output_batch(hypothesis)
         # else:
@@ -81,4 +89,3 @@ def generate_autoregressivly_gpt2_with_uq(
     assert token_ids.shape == (batch_size, max_tokens)
 
     return token_ids, uqs
-
