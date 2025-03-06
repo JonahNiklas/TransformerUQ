88ea4049d56017bcacea8c1143ae011eaf177df4
Sondre Sørbye <sondre@magson.no>
2025-03-06 13:53:47 +0100

    fix: attention is not using dropout during test time dropout

--- models/transformer.py
+++ models/transformer.py
@@ -24,7 +24,7 @@ class MultiHeadAttention(nn.Module):
 
         self.out = nn.Linear(d_model, d_model)
 
-        self.p_dropout = p_dropout
+        self.attention_dropout = nn.Dropout(p_dropout)
 
     def forward(
         self, query: Tensor, key: Tensor, value: Tensor, mask: Tensor
@@ -50,7 +50,7 @@ class MultiHeadAttention(nn.Module):
             V,
             attn_mask=mask,
             dropout_p=(
-                hyperparameters.transformer.dropout if self.training else 0.0
+                self.attention_dropout.p if self.attention_dropout.training else 0.0
             ),  # green dropout
         )
 
